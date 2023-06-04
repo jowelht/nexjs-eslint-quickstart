282a3ed8cb15de8c10bb14809dadc7b4da9a7066
jowelht <jowel@hastechit.com>
2023-06-04 10:33:43 +0600

    husky added

--- src/compoents/blog.jsx
+++ src/compoents/blog.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const BlogCard = ({ title, subTitle, id }) => {
     return (
-        <div>
-            <h1>{title}</h1>
-            <p>{subTitle}</p>
-        </div>
-    );
+<div>
+<h1>{title}</h1>
+<p>{subTitle}</p>
+</div>
+);
 };
 
 export default BlogCard;

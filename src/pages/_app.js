9d2b2cde893c24ed87054b5045c8943754032670
jowelht <jowel@hastechit.com>
2023-06-04 09:38:26 +0600

    Need to disable some ESLint rules error fixed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '@/styles/globals.css';
 
+// eslint-disable-next-line react/function-component-definition
 export default function App({ Component, pageProps }) {
     return <Component {...pageProps} />;
 }
